af414ff0674a342e46e9ef4a593884e3def373f2
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2016-07-19 10:36:33 +0200

    Moved all tasks to own module since chained tasks otherwise would not reach the workers. I guess it has something to do with the routing depending on module names, which confused me for a good while.

--- tasks/galaxy/tasks.py
+++ tasks/galaxy/tasks.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import json
 from time import sleep
@@ -8,11 +9,11 @@ from tasks import config
 from tasks.galaxy import galaxydata
 from tasks.galaxy.util import get_galaxy_instance
 
-TESTING_FLAG = True
+TESTING_FLAG = False
 
 # worker module for running workflow mods
 
-app = Celery('galaxy_workflows', backend='amqp')
+app = Celery('galaxy', backend='amqp')
 app.conf.update(
     BROKER_HOST=config.BROKER_URL,
     BROKER_PORT=config.BROKER_PORT,
@@ -123,6 +124,53 @@ def cleanup(inputstore):
     pass
     # removes analysis history, mzMLs will be left on disk bc they will be in
     # another history
+
+
+@app.task(queue=config.QUEUE_GALAXY_RESULT_TRANSFER)
+def download_result(inputstore):
+    """Downloads both zipped collections and normal datasets. This is a
+    task which can occupy the worker for a long time, since it waits for all
+    downloadable datasets to be completed"""
+    print('Got command to download results to disk from Galaxy for history '
+          '{}'.format(inputstore['history']))
+    gi = get_galaxy_instance(inputstore)
+    workflow_ok = True
+    while workflow_ok and False in [x['download_id'] for x in
+                                    inputstore['output_dsets'].values()]:
+        print('Datasets not ready yet, checking')
+        workflow_ok = check_output_datasets_wf(gi, inputstore)
+        sleep(60)
+    if workflow_ok:
+        print('Datasets ready, proceeding to download')
+        for dset in inputstore['output_dsets'].values():
+            dirname = os.path.dirname(dset['download_dest'])
+            if not os.path.exists(dirname) or not os.path.isdir(dirname):
+                os.makedirs(os.path.dirname(dset['download_dest']))
+            gi.datasets.download_dataset(dset['download_id'],
+                                         file_path=dset['download_dest'],
+                                         use_default_filename=False)
+    return inputstore
+
+
+def check_output_datasets_wf(gi, inputstore):
+    """Checks if to-download datasets in workflow are finished, sets their API
+    ID as download_id if they are ready, returns workflow_ok status in case
+    they are deleted/crashed (False) or not (True)"""
+    for dset in inputstore['output_dsets'].values():
+        if dset['download_id'] is not False:
+            # already checked this dataset
+            continue
+        try:
+            download_id = dset['packaged']
+        except KeyError:
+            download_id = dset['id']
+        dset_info = gi.datasets.show_dataset(download_id)
+        if dset_info['state'] == 'ok' and not dset_info['deleted']:
+            dset['download_id'] = download_id
+        elif dset_info['state'] == 'error' or dset_info['deleted']:
+            # Workflow crashed or user intervened, abort downloading
+            return False
+        return True
 
 
 def get_input_labels(wf):
