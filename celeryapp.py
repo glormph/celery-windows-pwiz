c8f63a22f57917050dec8b7b05da81b40105d32e
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2016-08-29 12:19:18 +0200

    Celery app in separate module I forgot to check in

---
+++ celeryapp.py
@@ -0,0 +1,13 @@
+from celery import Celery
+from tasks import config
+
+
+app = Celery('proteomics-tasks')
+app.conf.update(
+    BROKER_HOST=config.BROKER_URL,
+    BROKER_PORT=config.BROKER_PORT,
+    CELERY_TASK_SERIALIZER=config.CELERY_TASK_SERIALIZER,
+    CELERY_RESULT_SERIALIZER=config.CELERY_TASK_SERIALIZER,
+    CELERY_ACCEPT_CONTENT=[config.CELERY_TASK_SERIALIZER],
+    CELERY_RESULT_BACKEND=config.CELERY_RESULT_BACKEND,
+)
