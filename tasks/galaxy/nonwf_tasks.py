2d45c896b191562fecffab7a225977963ff95f1d
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2016-12-21 10:49:28 +0100

    Updated galaxydata on workflows

--- tasks/galaxy/nonwf_tasks.py
+++ tasks/galaxy/nonwf_tasks.py
@@ -37,6 +37,28 @@ tasks = {'@pout2mzid': {'task': runtasks.run_pout2mzid_on_sets,
                                      'params': [],
                                      'outputs': ['spectra'],
                                      },
+         '@create_6rf_split_dbs': {'task': runtasks.create_6rf_split_dbs,
+                                     'inputs': ['peptable MS1 deltapi'],
+                                     'params': ['fr_widthlist', 'interceptlist',
+                                                'fr_amounts', 'pi_tolerances',
+                                                'reverses',
+                                                'setpatterns', 'setnames',
+                                                'strips', 'strippatternlist'],
+                                     # outputs have dynamic names FIXME make dict of it
+                                     # maybe [name][td] = {'src': 'hdca', 'id': 1234}
+                                     'outputs': [''],
+                                     },
+         '@create_spectra_db_pairedlist': {
+                                  'task': runtasks.create_spectra_db_pairedlist,
+                                  # FIXME there are more inputs but they are
+                                  # dynamically named after the set/pi
+                                  'inputs': ['spectra'],
+                                  'params': ['code',
+                                             'setpatterns',
+                                             'strips', 'strippatternlist'],
+                                  'outputs': ['spectra target db',
+                                              'spectra decoy db'],
+                                     },
          }
 
 
