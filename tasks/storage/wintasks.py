014ec384790df636e75900e8e5e6b33fcbacf24a
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-09-12 12:01:42 +0200

    New task for scp directly from conversion box as we have new boxes

--- tasks/storage/wintasks.py
+++ tasks/storage/wintasks.py
@@ -2,19 +2,62 @@ import sys
 import os
 import subprocess
 import shutil
+from hashlib import md5
+
 
 from celeryapp import app
 from tasks import config
 
-# import task to chain
+# import task to chain, could poss be removed
 from tasks.storage import scp
 
 
 PROTEOWIZ_LOC = ('C:\Program Files\ProteoWizard\ProteoWizard '
                  '3.0.6002\msconvert.exe')
+PSCP_LOC = ('C:\Program Files\Putty')
 RAWDUMPS = 'C:\\rawdump'
 MZMLDUMPS = 'C:\\mzmldump'
 OUTBOX = 'X:'
+
+
+@app.task(queue=config.QUEUE_SCP, bind=True)
+def tmp_scp_storage(self, inputstore):
+    mzmlfile = os.path.join(MZMLDUMPS, inputstore['mzml'])
+    hashmd5 = md5()
+    with open(mzmlfile, 'rb') as fp:
+        for chunk in iter(lambda: fp.read(4096), b''):
+            hashmd5.update(chunk)
+    mzml_md5 = hashmd5.hexdigest()
+    print('Copying mzML file {} with md5 {} to storage'.format(
+        inputstore['mzml'], mzml_md5))
+    dstfolder = os.path.join(inputstore['winshare'],
+                             inputstore['current_storage_dir'])
+    dst = '{}@{}'.format(config.SCP_LOGIN, dstfolder)
+    try:
+        subprocess.check_call([PSCP_LOC, '-i', config.PUTTYKEY, mzmlfile, dst])
+    except:
+        # FIXME probably better to not retry? put in dead letter queue?
+        # usually when this task has probelsm it is usually related to network
+        # or corrupt file, both of which are not nice to retry
+        self.retry(countdown=60)
+    arrived_file = os.path.join(dstfolder, os.path.basename(mzmlfile))
+    try:
+        with open(arrived_file, 'rb') as fp:
+            for chunk in iter(lambda: fp.read(4096), b''):
+                hashmd5.update(chunk)
+        dst_md5 = hashmd5.hexdigest()
+    except:
+        # FIXME probably better to not retry? put in dead letter queue?
+        # usually when this task has probelsm it is usually related to network
+        self.retry(countdown=60)
+    if not dst_md5 == mzml_md5:
+        self.retry(countdown=60)
+        return
+    inputstore['mzml_md5'] = mzml_md5
+    inputstore['mzml_path'] = dst
+    print('done with {}'.format(inputstore['mzml']))
+    os.remove(mzmlfile)
+    return inputstore
 
 
 @app.task(bind=True, queue=config.QUEUE_CONVERSION)
